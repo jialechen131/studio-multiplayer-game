74697bf3a54b4c4774b8aba850467a0095ff2666
Gary <jialechen321@gmail.com>
2019-04-10 18:08:17 -0400

    Testing
    .....

--- src/gameData.js
+++ src/gameData.js
@@ -1,25 +1,23 @@
-import ChatRoom from './games/chatroom/ChatRoom.js';
-import TicTacToe from './games/tictactoe/TicTacToe.js';
-import RockPaperScissors from './games/rps/RockPaperScissors.js';
-import Multiplication from './games/multiplication/Multiplication.js';
-import Pong from './games/pong/Pong.js';
-import TwentyOneQuestions from './games/twentyOneQuestions/TwentyOneQuestions.js';
-import Spark from './games/spark/Spark.js';
-import uno from './games/uno/uno.js';
-import Mafia from './games/mafia/Mafia.js';
-import Pokemon from './games/pokemon/Pokemon.js';
-import FFW from './games/FFW/FFW.js';
-
+import ChatRoom from "./games/chatroom/ChatRoom.js";
+import TicTacToe from "./games/tictactoe/TicTacToe.js";
+import RockPaperScissors from "./games/rps/RockPaperScissors.js";
+import Multiplication from "./games/multiplication/Multiplication.js";
+import Pong from "./games/pong/Pong.js";
+import TwentyOneQuestions from "./games/twentyOneQuestions/TwentyOneQuestions.js";
+import Spark from "./games/spark/Spark.js";
+import uno from "./games/uno/uno.js";
+import Mafia from "./games/mafia/Mafia.js";
+import Pokemon from "./games/pokemon/Pokemon.js";
+import FFW from "./games/FFW/FFW.js";
 
 const gameData = {
-
   chatroom: {
     title: "Chat Room",
     authors: "Joe Tessler",
     description: "A place to chat with a group of friends",
     minUsers: 1,
     maxUsers: 10,
-    component: ChatRoom,
+    component: ChatRoom
   },
 
   tictactoe: {
@@ -28,7 +26,7 @@ const gameData = {
     description: "The classic two-player game with Xs and Os",
     minUsers: 2,
     maxUsers: 2,
-    component: TicTacToe,
+    component: TicTacToe
   },
 
   rockpaperscissors: {
@@ -37,7 +35,7 @@ const gameData = {
     description: "Class 2-player rock paper scissors",
     minUsers: 2,
     maxUsers: 2,
-    component: RockPaperScissors,
+    component: RockPaperScissors
   },
 
   multiplication: {
@@ -46,7 +44,7 @@ const gameData = {
     description: "A multiplication speed game",
     minUsers: 2,
     maxUsers: 2,
-    component: Multiplication,
+    component: Multiplication
   },
 
   pong: {
@@ -55,16 +53,16 @@ const gameData = {
     description: "Classic Pong",
     minUsers: 1,
     maxUsers: 2,
-    component: Pong,
+    component: Pong
   },
 
   pokemon: {
-      title: "Pokemon Simulator",
-      authors: "Imani Greenidge and Raishan Bernard",
-      description: "Turn Based Pokemon Battle",
-      minUsers: 1,
-      maxUsers: 2,
-      component: Pokemon,
+    title: "Pokemon Simulator",
+    authors: "Imani Greenidge and Raishan Bernard",
+    description: "Turn Based Pokemon Battle",
+    minUsers: 1,
+    maxUsers: 2,
+    component: Pokemon
   },
 
   questions: {
@@ -73,7 +71,7 @@ const gameData = {
     description: "21 questions between 2 players",
     minUsers: 1,
     maxUsers: 2,
-    component: TwentyOneQuestions,
+    component: TwentyOneQuestions
   },
 
   photosharing: {
@@ -82,7 +80,7 @@ const gameData = {
     description: "Ripping off Twitter, Instagram, AND Tumblr.",
     minUsers: 2,
     maxUserss: 5,
-    component: Spark,
+    component: Spark
   },
 
   uno: {
@@ -91,26 +89,27 @@ const gameData = {
     description: "The classic UNO game",
     minUsers: 2,
     maxUsers: 2,
-    component: uno,
+    component: uno
   },
 
   mafia: {
     title: "Mafia",
     authors: "Caelan Springer, Daniel Lin, Daniel Zamilatskiy",
-    description: "Your greatest friends can be your greatest enemy. Trust no one but yourself.",
+    description:
+      "Your greatest friends can be your greatest enemy. Trust no one but yourself.",
     minUsers: 1,
     maxUsers: 20,
-    component: Mafia,
+    component: Mafia
   },
-  
+
   frostFireWater: {
-    title: "Frost Fire Watter",
+    title: "Frost Fire Water",
     authors: "Daniel Zamilatskiy, Gary Chen",
     description: "",
     minUsers: 2,
     maxUsers: 2,
-    component: FFW,
+    component: FFW
   }
-}
+};
 
 export default gameData;
--- src/games/FFW/FFW.js
+++ src/games/FFW/FFW.js
@@ -1,22 +1,26 @@
-import Button from '@material-ui/core/Button';
-import GameComponent from '../../GameComponent.js';
-import React from 'react';
-import UserApi from '../../UserApi.js';
-import Boostrap from 'react-bootstrap/Button';
+import Button from "@material-ui/core/Button";
+import GameComponent from "../../GameComponent.js";
+import React from "react";
+import UserApi from "../../UserApi.js";
+// import Boostrap from 'react-bootstrap/Button';
 
 export default class FFW extends GameComponent {
-    render() {
-        var id = this.getSessionId();
-        var users = this.getSessionUserIds().map((user_id) => (<li key={user_id}>{ user_id }</li>));
-        var creator = this.getSessionCreatorUserId();
-        return (
-            <div>
-                <p>Session ID: {id}</p>
-                <p>Session creator: {creator}</p>
-                <p>Session users:</p>
-                <ul> {users} </ul>
-            </div>
-
-        );
-    }
+  render() {
+    var id = this.getSessionId();
+    var creatorId = this.getSessionCreatorUserId();
+    var users = this.getSessionUserIds().map(user_id => (
+      <li key={user_id}>{UserApi.getName(user_id)}</li>
+    ));
+    var creator = UserApi.getName(creatorId);
+    var isHost = this.getMyUserId() === creatorId;
+    return (
+      <div>
+        <h3>Hello, {isHost ? `Host` : `Guest`}</h3>
+        <p>Session ID: {id}</p>
+        <p>Session creator: {creator}</p>
+        <p>Session users:</p>
+        <ul> {users} </ul>
+      </div>
+    );
+  }
 }
