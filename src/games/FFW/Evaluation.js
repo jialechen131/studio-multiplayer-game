abf8c035b9a76ca3306940caf971f2b23efb9bcb
Undefined8 <dZamilatskiy2019@bklawtech.com>
2019-05-22 21:45:28 +0000

    MVP

--- src/games/FFW/Evaluation.js
+++ src/games/FFW/Evaluation.js
@@ -5,19 +5,51 @@ import UserApi from "../../UserApi.js";
 
 export default class Evaluation extends React.Component {
 
+    getWinner(userName, userChoice, opponentName, opponentChoice) {
+        if(userChoice === "Fire"){
+            if(opponentChoice === "Fire"){
+                return "Nobody";
+            } else if (opponentChoice === "Frost") {
+                return userName;
+            } else if (opponentChoice === "Frost") {
+                return opponentName;
+            }
+        } else if(userChoice === "Frost"){
+            if(opponentChoice === "Fire"){
+                return opponentName;
+            } else if (opponentChoice === "Frost") {
+                return "Nobody";
+            } else if (opponentChoice === "Frost") {
+                return userName;
+            }
+        } else if(userChoice === "Water"){
+            if(opponentChoice === "Fire"){
+                return userName;
+            } else if (opponentChoice === "Frost") {
+                return opponentName;
+            } else if (opponentChoice === "Frost") {
+                return "Nobody";
+            }
+        }
+    }
 
     render() {
         console.log(this.props, "eval");
         const user = this.props.players[`${this.props.playerID}`];
         const userName = UserApi.getName(this.props.playerID);
+        const userChoice = user.selection;
         const opponent = this.props.players[`${this.props.opponentID}`];
         const opponentName = UserApi.getName(this.props.opponentID);
+        const opponentChoice = opponent.selection;
+
+        const winner = this.getWinner(userName, userChoice, opponentName, opponentChoice);
 
         if(user){
             return (<div>
 
                 <p>{userName}: {user.selection}</p>
                 <p>{opponentName}: {opponent.selection}</p>
+                <p>The winner is {winner}</p>
             </div>)
         }else{
             return (<div>Error</div>);
